105263f83b39f873dd6ff1d9d8ed5be39e37e0eb
VeronicaVassallo <vvassallo94@gmail.com>
2024-02-14 18:30:55 +0100

    delete product + wip put product

--- giarafe/src/pages/Backoffice.jsx
+++ giarafe/src/pages/Backoffice.jsx
@@ -2,20 +2,46 @@ import { useEffect, useState } from "react";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 import Addproduct from "../components/AddProduct";
+import env from "react-dotenv";
 import "../style.css";
 
 const Backoffice = () => {
-	const [allProducts, setAllProducts] = useState([]);
+	const [allProducts, setAllProducts] = useState([]); //prodotti tipologia specifica (es: solo le pizze)
 	const [typologyValue, setTypologyValue] = useState("pizza");
 	const [show, setShow] = useState(false);
+	const [idProductSelected, setIdProductSelected] = useState(""); //id prodotto selezionato
+	const [dataProductSelected, setDataProductSelected] = useState({});
 
 	const handleClose = () => setShow(false);
-	const handleShow = () => setShow(true);
 
+	const handleShow = (_id) => {
+		debugger;
+		setIdProductSelected(_id);
+		setShow(true);
+		const filterProduct = allProducts.filter((product) => product._id === _id);
+		setDataProductSelected(filterProduct[0]);
+	};
+
+	//Cancella prodotto specifico in base all 'id
+	const handleDeleteProduct = async () => {
+		const response = await fetch(
+			`${env.REACT_APP_SERVER_BASE_URL}/product/delete/${idProductSelected}`,
+			{
+				headers: {
+					"Content-Type": "application/json",
+				},
+
+				method: "DELETE",
+			}
+		);
+		window.location.reload();
+	};
+
+	//Ti richiama tutti i prodotti di una tipologia specifica
 	try {
 		const getData = async () => {
 			const responce = await fetch(
-				`http://localhost:5050/product/${typologyValue}`
+				`${env.REACT_APP_SERVER_BASE_URL}/product/${typologyValue}`
 			);
 			let data = await responce.json();
 
@@ -78,9 +104,9 @@ const Backoffice = () => {
 					</thead>
 					<tbody>
 						{allProducts &&
-							allProducts?.map((p) => {
+							allProducts.map((p) => {
 								return (
-									<tr key={p._id} onClick={handleShow}>
+									<tr key={p._id} onClick={() => handleShow(p._id)}>
 										<td>{p.productName}</td>
 										<td>{p.ingredients}</td>
 										<td>{p.price}</td>
@@ -103,7 +129,12 @@ const Backoffice = () => {
 				<Modal.Body>
 					<form className="d-flex flex-column">
 						<label for="typology">Seleziona tipologia prodotto:</label>
-						<select name="typology" id="typology">
+						<select
+							name="typology"
+							id="typology"
+							defaultValue={dataProductSelected.typology}
+							target={dataProductSelected.typology}
+						>
 							<option value="" disabled selected></option>
 							<option value="pizza">pizza</option>
 							<option value="antipasti rustici">antipasti rustici</option>
@@ -117,17 +148,37 @@ const Backoffice = () => {
 						</select>
 
 						<label htmlFor="productName"> Nome prodotto:</label>
-						<input type="text" name="productName" id="productName" />
+						<input
+							type="text"
+							name="productName"
+							id="productName"
+							defaultValue={dataProductSelected.productName}
+							target={dataProductSelected.productName}
+						/>
 						<label htmlFor="ingredients"> Ingredienti:</label>
-						<input type="text" name="ingredients" id="ingredients" />
+						<input
+							type="text"
+							name="ingredients"
+							id="ingredients"
+							defaultValue={dataProductSelected.ingredients}
+							target={dataProductSelected.ingredients}
+						/>
 						<label htmlFor="price"> Prezzo € :</label>
-						<input type="number" name="price" id="price" />
+						<input
+							type="number"
+							name="price"
+							id="price"
+							defaultValue={dataProductSelected.price}
+							target={dataProductSelected.price}
+						/>
 						<label htmlFor="img"> Cover:</label>
-						<input type="text" name="img" id="img" />
+						<input type="file" name="img" id="img" />
 					</form>
 				</Modal.Body>
 				<Modal.Footer>
-					<Button variant="danger">Elimina prodotto</Button>
+					<Button variant="danger" onClick={handleDeleteProduct}>
+						Elimina prodotto
+					</Button>
 					<Button variant="secondary" onClick={handleClose}>
 						Chiudi
 					</Button>
